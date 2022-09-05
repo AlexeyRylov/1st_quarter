c6f9223e92b5d79990fb321591b33df660cf62a7
alexey <aarylov@gmail.com>
2022-09-05 16:57:37 +0500

    Creation code

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿int CalcOutSize(string[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3) { count += 1; }
+    }
+    return count;
+}
+
+string[] FillingResultArray(string[] inArray, string[] outArray)
+{
+    int j = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3)
+        {
+            outArray[j] = inArray[i];
+            j++;
+        }
+    }
+    return outArray;
+}
+
+void PrintArray(string[] outString)
+{
+    for (int i = 0; i < outString.Length; i++)
+    {
+        Console.Write(outString[i] + "\t");
+    }
+}
+
+Console.WriteLine("Введите строки, разделите их пробелами: ");
+string initString = Console.ReadLine();
+string[] initSplit = initString.Split(' ');
+
+int sizeOut = CalcOutSize(initSplit);
+
+string[] outString = new string[sizeOut];
+outString = FillingResultArray(initSplit, outString);
+
+PrintArray(outString);
+
+
